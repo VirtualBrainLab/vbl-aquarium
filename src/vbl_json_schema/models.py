908d32ff06e137f25ad656efd5b4641ba9c8c6dc
Kenneth Yang <kjy5@uw.edu>
2024-03-07 13:16:14 -0800

    Use json dumps

--- src/vbl_json_schema/models.py
+++ src/vbl_json_schema/models.py
@@ -1,4 +1,8 @@
+from json import loads, dumps
+
+from jsonschema import validate
 from pydantic import BaseModel
+from requests import get
 
 
 class Vector3(BaseModel):
@@ -13,4 +17,12 @@ class Vector3Data(BaseModel):
 
 
 with open("./schemas/vector3.json", "w") as f:
-    f.write(str(Vector3.model_json_schema()))
+    f.write(dumps(Vector3.model_json_schema()))
+
+online_schema = get(
+    "https://raw.githubusercontent.com/VirtualBrainLab/vbl-json-schema/pydantic/src/vbl_json_schema/schemas/vector3.json")
+print(online_schema.text)
+as_dict = loads(online_schema.text)
+
+print(validate(instance={"id": 1, "value": {"x": 1.0, "y": 2.0, "z": 3.0}},
+               schema=as_dict))
