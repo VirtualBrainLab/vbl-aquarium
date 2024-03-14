5ede0aa70974719063d6690cc43b1c8c02d18489
Kenneth Yang <kjy5@uw.edu>
2024-03-14 08:52:14 -0700

    Working on comments

--- src/vbl_aquarium/models/ephys_link.py
+++ src/vbl_aquarium/models/ephys_link.py
@@ -4,6 +4,15 @@ from vbl_aquarium.models_unity import Vector4
 
 
 class GotoPositionRequest(BaseModel):
+    """Data format for requesting a manipulator to move to a position.
+    
+    :param manipulator_id: ID of the manipulator to move.
+    :type manipulator_id: str
+    :param position: Position to move to in mm (X, Y, Z, W).
+    :type position: Vector4
+    :param speed: Speed to move at in mm/s.
+    :type speed: float
+    """
     manipulator_id: str
     position: Vector4
     speed: float
