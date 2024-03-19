61efc37cf4aa68446dad40270a9dcaec6e2a4b77
Kenneth Yang <kjy5@uw.edu>
2024-03-18 20:15:17 -0700

    Created VBLBaseModel and more Ephys Link models

--- src/vbl_aquarium/models/ephys_link.py
+++ src/vbl_aquarium/models/ephys_link.py
@@ -1,11 +1,16 @@
-from pydantic import BaseModel
+from __future__ import annotations
 
-from vbl_aquarium.models_unity import Vector4
+from typing import TYPE_CHECKING
+
+from vbl_aquarium.models.vbl_base_model import VBLBaseModel
+
+if TYPE_CHECKING:
+    from vbl_aquarium.models_unity import Vector3, Vector4
 
 
-class GotoPositionRequest(BaseModel):
-    """Data format for requesting a manipulator to move to a position.
-    
+class GotoPositionRequest(VBLBaseModel):
+    """Request format for moving a manipulator to a position.
+
     :param manipulator_id: ID of the manipulator to move.
     :type manipulator_id: str
     :param position: Position to move to in mm (X, Y, Z, W).
@@ -13,6 +18,82 @@ class GotoPositionRequest(BaseModel):
     :param speed: Speed to move at in mm/s.
     :type speed: float
     """
+
     manipulator_id: str
     position: Vector4
     speed: float
+
+
+class InsideBrainRequest(VBLBaseModel):
+    """Request format for setting inside brain state.
+
+    :param manipulator_id: ID of the manipulator to move.
+    :type manipulator_id: str
+    :param inside: Whether the manipulator is inside the brain.
+    :type inside: bool
+    """
+
+    manipulator_id: str
+    inside: bool
+
+
+class DriveToDepthRequest(VBLBaseModel):
+    """Request format for driving a manipulator to depth.
+
+    :param manipulator_id: ID of the manipulator to move.
+    :type manipulator_id: str
+    :param depth: Depth to drive to in mm.
+    :type depth: float
+    :param speed: Speed to drive at in mm/s.
+    :type speed: float
+    """
+
+    manipulator_id: str
+    depth: float
+    speed: float
+
+
+class CanWriteRequest(VBLBaseModel):
+    """Request format for setting can write state.
+
+    :param manipulator_id: ID of the manipulator to move.
+    :type manipulator_id: str
+    :param can_write: Whether the manipulator can write.
+    :type can_write: bool
+    :param hours: Number of hours the manipulator can write for.
+    :type hours: float
+    """
+
+    manipulator_id: str
+    can_write: bool
+    hours: float
+
+
+class GetManipulatorsResponse(VBLBaseModel):
+    """Response format for requesting available manipulators.
+
+    :param manipulators: List of manipulators.
+    :type manipulators: list[str]
+    :param num_axes: Number of axes for the manipulators.
+    :type num_axes: int
+    :param dimensions: Dimensions of the manipulators (first 3 axes in unified manipulator space).
+    :type dimensions: Vector3
+    :param error: Error message if any.
+    :type error: str
+    """
+
+    manipulators: list[str]
+    num_axes: int
+    dimensions: Vector3
+    error: str
+
+
+class PositionalResponse(VBLBaseModel):
+    """Response format for the manipulator position.
+
+    :param position: Position of the manipulator.
+    :type position: Vector4
+    """
+
+    position: Vector4
+    error: str
