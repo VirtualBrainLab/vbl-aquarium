5a661e3a14b796821904209fb4efdc402cf0358e
kjy5 <82800265+kjy5@users.noreply.github.com>
2024-03-21 18:49:31 +0000

    Build Models

--- models/csharp/ephys_link/GetManipulatorsResponse.cs
+++ models/csharp/ephys_link/GetManipulatorsResponse.cs
@@ -5,6 +5,6 @@ public struct GetManipulatorsResponse
 {
     public string[] Manipulators;
     public int NumAxes;
-    public Vector3 Dimensions;
+    public Vector4 Dimensions;
     public string Error;
 }
