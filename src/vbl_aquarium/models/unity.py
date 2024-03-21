929ed80b2fc7954db7afe9b8f874c7b3ec6b5209
Kenneth Yang <kjy5@uw.edu>
2024-03-21 13:29:42 -0700

    Add math functions to Vector4

--- src/vbl_aquarium/models/unity.py
+++ src/vbl_aquarium/models/unity.py
@@ -27,3 +27,27 @@ class Vector4(BaseModel):
     y: float = 0.0
     z: float = 0.0
     w: float = 0.0
+
+    def __add__(self, other):
+        if isinstance(other, Vector4):
+            return Vector4(x=self.x + other.x, y=self.y + other.y, z=self.z + other.z, w=self.w + other.w)
+        error = f"unsupported operand type(s) for +: 'Vector4' and '{type(other)}'"
+        raise TypeError(error)
+
+    def __sub__(self, other):
+        if isinstance(other, Vector4):
+            return Vector4(x=self.x - other.x, y=self.y - other.y, z=self.z - other.z, w=self.w - other.w)
+        error = f"unsupported operand type(s) for -: 'Vector4' and '{type(other)}'"
+        raise TypeError(error)
+
+    def __mul__(self, other):
+        if isinstance(other, (int, float)):
+            return Vector4(x=self.x * other, y=self.y * other, z=self.z * other, w=self.w * other)
+        error = f"unsupported operand type(s) for *: 'Vector4' and '{type(other)}'"
+        raise TypeError(error)
+
+    def __truediv__(self, other):
+        if isinstance(other, (int, float)):
+            return Vector4(x=self.x / other, y=self.y / other, z=self.z / other, w=self.w / other)
+        error = f"unsupported operand type(s) for /: 'Vector4' and '{type(other)}'"
+        raise TypeError(error)
