c7d08d1eb9a9349b00fa52a9beaf2fa168d40619
kjy5 <82800265+kjy5@users.noreply.github.com>
2024-03-27 17:17:14 +0000

    Build Models

--- models/csharp/urchin/StructureModel.cs
+++ models/csharp/urchin/StructureModel.cs
@@ -6,8 +6,8 @@ public struct StructureModel
     public string Name;
     public string Acronym;
     public int AtlasId;
-    public bool Visible;
     public Color Color;
+    public bool Visible;
     public float ColorIntensity;
     public int Side;
     public string Material;
